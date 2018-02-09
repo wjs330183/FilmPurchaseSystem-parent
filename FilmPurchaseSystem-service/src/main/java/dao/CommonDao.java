deb673905a58f26a2de6c9fd49ef4e0703682178
wjsjj <wangjs@hzzh.com>
2018-02-09 11:46:59 +0800

    customer创建

--- FilmPurchaseSystem-service/src/main/java/dao/CommonDao.java
+++ FilmPurchaseSystem-service/src/main/java/dao/CommonDao.java
@@ -3,13 +3,14 @@ package dao;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface CommonDao {
 
     String getTable(@Param("tableName") String tableName);
 
-    int batchCreateTable(@Param("commonList") List commonList);
+    int batchCreateTable(@Param("tableName") String tableName, @Param("datas") List<Map<String, String>> datas);
 
-    int batchUpdate(@Param("commonList") List commonList);
+    int batchUpdate(@Param("tableName") String tableName, @Param("information") String information, @Param("datas") List<Map<String, String>> datas);
 
 }
